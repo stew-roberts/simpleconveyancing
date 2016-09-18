2a937c0cfe11e9f54bdace1edc55a43c96a289df
Stewart Roberts <stew.roberts@gmail.com>
2016-09-18 13:15:49 +0000

    Added api Key

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-	$apiKey = '76261671-11f4-47e1-be77-3e5d461886be';
+	$apiKey = '76261671-11f4-47e1-be77-3e5d461886b';
     $pageData = json_decode(file_get_contents('http://contentmanagement.streamwiredigital.com/index.php/api/format/json/apikey/' . $apiKey ));
 ?>
 <!DOCTYPE html>
